23c51c511871dcec6b479290ca931a4e2d12f525
NayanTheSpaceGuy <147062173+NayanTheSpaceGuy@users.noreply.github.com>
2024-08-31 02:39:38 +0530

    this is a binary crate so no more tests dir

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ mod controller;
 mod commands;
 mod logger;
 mod handlers;
-mod tests;
 
 use color_eyre::eyre::Result;
 use controller::init_cli_controller;
--- src/tests/cli_tests.rs
+++
@@ -1,9 +0,0 @@
-#[cfg(test)]
-use clap::CommandFactory;
-#[cfg(test)]
-use crate::controller::Cli;
-
-#[test]
-pub fn verify_cli() {
-    Cli::command().debug_assert();
-}
