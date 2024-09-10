50729c70cecfe528e0acb40daadc31e1f267edee
NayanTheSpaceGuy <147062173+NayanTheSpaceGuy@users.noreply.github.com>
2024-09-11 01:12:35 +0530

    added some TODOs :)

--- src/commands/lvl_1/app.rs
+++ src/commands/lvl_1/app.rs
@@ -2,8 +2,10 @@ use clap::Args;
 
 #[derive(Args, Debug)]
 pub struct AppCommand {
-
+    // TODO: turn this into a subcommand and handle it
+    /*
     /// See list of available system utilities
     #[arg(short = 'l', long, exclusive = true)]
     pub list_apps: bool,
+    */
 }
--- src/commands/lvl_1/system.rs
+++ src/commands/lvl_1/system.rs
@@ -3,7 +3,10 @@ use clap::Args;
 #[derive(Args, Debug)]
 pub struct SystemCommand {
 
+    // TODO: turn this into a subcommand and handle it
+    /*
     /// See list of available system utilities
     #[arg(short = 'l', long, exclusive = true)]
     pub list_utils: bool,
+    */
 }
