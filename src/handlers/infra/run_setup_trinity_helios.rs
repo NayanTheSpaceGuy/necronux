c910a51463f86124f5fb6071f67862d1129c904b
NayanTheSpaceGuy <147062173+NayanTheSpaceGuy@users.noreply.github.com>
2024-09-21 00:52:47 +0530

    Refactor to clone dotlab repo into config directory instead of bundling scripts

--- src/handlers/infra/run_setup_trinity_helios.rs
+++ src/handlers/infra/run_setup_trinity_helios.rs
@@ -1,38 +1,85 @@
 use color_eyre::eyre::{eyre, Result};
 use log::debug;
 #[cfg(unix)]
+use std::{env, path::PathBuf, process::Command};
 use std::{fs::Permissions, os::unix::fs::PermissionsExt};
-use std::{path::PathBuf, process::Command};
 
 pub fn trinity_helios_part_one() -> Result<()> {
-    let project_root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    let script_path = project_root.join("src/scripts/infra/trinity_helios_setup_part_one.sh");
-
-    debug!(
-        "Attempting to change permissions of script: {:?}",
-        script_path
-    );
-
     #[cfg(unix)]
     {
-        std::fs::set_permissions(&script_path, Permissions::from_mode(0o755))?;
+        let config_dir: PathBuf =
+            env::var("XDG_CONFIG_HOME")
+                .map(PathBuf::from)
+                .or_else(|_| -> Result<PathBuf> {
+                    let home_dir = env::var("HOME")
+                        .map(PathBuf::from)
+                        .map_err(|_| eyre!("HOME environment variable not set"))?;
+                    Ok(home_dir.join(".config"))
+                })?;
+
+        let project_dir = config_dir.join("necronux");
+        if !project_dir.exists() {
+            std::fs::create_dir_all(&project_dir)?;
+            debug!("Created project directory at {:?}", project_dir);
+        }
+
+        let dot_dir = project_dir.join("dot");
+        if !dot_dir.exists() {
+            std::fs::create_dir_all(&dot_dir)?;
+            debug!("Created 'dot' directory at {:?}", dot_dir);
+        }
+
+        let repo_url = "https://github.com/NayanTheSpaceGuy/dotlab.git";
+        let git_clone_status = Command::new("git")
+            .arg("clone")
+            .arg(repo_url)
+            .arg(&dot_dir)
+            .status()
+            .map_err(|e| eyre!("Failed to execute git command: {:?}", e))?;
+
+        if !git_clone_status.success() {
+            return Err(eyre!(
+                "Git clone failed with status: {:?}",
+                git_clone_status
+            ));
+        }
+        debug!("Successfully cloned repository into {:?}", project_dir);
+
+        let script_path = dot_dir.join("homelab/scripts/trinity_helios_setup_part_one.sh");
+
+        debug!(
+            "Attempting to change permissions of script: {:?}",
+            script_path
+        );
+        std::fs::set_permissions(&script_path, Permissions::from_mode(0o755)).map_err(|e| {
+            eyre!(
+                "Failed to set permissions for script {:?}: {:?}",
+                script_path,
+                e
+            )
+        })?;
         debug!("Successfully changed permissions of the script");
+
+        let script_str = script_path
+            .to_str()
+            .ok_or_else(|| eyre!("Failed to convert script path {:?} to string", script_path))?;
+
+        debug!("Executing script: {}", script_str);
+
+        let execute = Command::new("sh")
+            .arg("-c")
+            .arg(script_str)
+            .status()
+            .map_err(|e| eyre!("Failed to execute script {:?}: {:?}", script_str, e))?;
+
+        if !execute.success() {
+            return Err(eyre!("Script execution failed with status: {:?}", execute));
+        }
     }
+
     #[cfg(windows)]
     {
-        println!("Permission setting is not implemented yet for Windows.");
-    }
-
-    debug!("Preparing to run trinity_helios_part_one script");
-    let script_str = script_path
-        .to_str()
-        .ok_or_else(|| eyre!("Failed to convert script path to string"))?;
-
-    debug!("Executing script: {}", script_str);
-    let execute = Command::new("sh").arg("-c").arg(script_str).status()?;
-
-    if !execute.success() {
-        return Err(eyre!("Script execution failed with status: {}", execute));
+        println!("This setup is not supported yet for Windows.");
     }
 
     debug!("Script executed successfully");
@@ -40,39 +87,81 @@ pub fn trinity_helios_part_one() -> Result<()> {
 }
 
 pub fn trinity_helios_part_two() -> Result<()> {
-    // Check if the OS is Linux before executing the script
-    if std::env::consts::OS != "linux" {
-        return Ok(()); // Skip execution if not on Linux
-    }
-
-    let project_root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    let script_path = project_root.join("src/scripts/infra/trinity_helios_setup_part_two.sh");
-
-    debug!(
-        "Attempting to change permissions of script: {:?}",
-        script_path
-    );
-
     #[cfg(unix)]
     {
-        std::fs::set_permissions(&script_path, Permissions::from_mode(0o755))?;
+        let config_dir: PathBuf =
+            env::var("XDG_CONFIG_HOME")
+                .map(PathBuf::from)
+                .or_else(|_| -> Result<PathBuf> {
+                    let home_dir = env::var("HOME")
+                        .map(PathBuf::from)
+                        .map_err(|_| eyre!("HOME environment variable not set"))?;
+                    Ok(home_dir.join(".config"))
+                })?;
+
+        let project_dir = config_dir.join("necronux");
+        if !project_dir.exists() {
+            std::fs::create_dir_all(&project_dir)?;
+            debug!("Created project directory at {:?}", project_dir);
+        }
+
+        let dot_dir = project_dir.join("dot");
+        if !dot_dir.exists() {
+            std::fs::create_dir_all(&dot_dir)?;
+            debug!("Created 'dot' directory at {:?}", dot_dir);
+        }
+
+        let repo_url = "https://github.com/NayanTheSpaceGuy/dotlab.git";
+        let git_clone_status = Command::new("git")
+            .arg("clone")
+            .arg(repo_url)
+            .arg(&dot_dir)
+            .status()
+            .map_err(|e| eyre!("Failed to execute git command: {:?}", e))?;
+
+        if !git_clone_status.success() {
+            return Err(eyre!(
+                "Git clone failed with status: {:?}",
+                git_clone_status
+            ));
+        }
+        debug!("Successfully cloned repository into {:?}", project_dir);
+
+        let script_path = dot_dir.join("homelab/scripts/trinity_helios_setup_part_two.sh");
+
+        debug!(
+            "Attempting to change permissions of script: {:?}",
+            script_path
+        );
+        std::fs::set_permissions(&script_path, Permissions::from_mode(0o755)).map_err(|e| {
+            eyre!(
+                "Failed to set permissions for script {:?}: {:?}",
+                script_path,
+                e
+            )
+        })?;
         debug!("Successfully changed permissions of the script");
+
+        let script_str = script_path
+            .to_str()
+            .ok_or_else(|| eyre!("Failed to convert script path {:?} to string", script_path))?;
+
+        debug!("Executing script: {}", script_str);
+
+        let execute = Command::new("sh")
+            .arg("-c")
+            .arg(script_str)
+            .status()
+            .map_err(|e| eyre!("Failed to execute script {:?}: {:?}", script_str, e))?;
+
+        if !execute.success() {
+            return Err(eyre!("Script execution failed with status: {:?}", execute));
+        }
     }
+
     #[cfg(windows)]
     {
-        println!("Permission setting is not implemented yet for Windows.");
-    }
-
-    debug!("Preparing to run trinity_helios_part_two script");
-    let script_str = script_path
-        .to_str()
-        .ok_or_else(|| eyre!("Failed to convert script path to string"))?;
-
-    debug!("Executing script: {}", script_str);
-    let execute = Command::new("sh").arg("-c").arg(script_str).status()?;
-
-    if !execute.success() {
-        return Err(eyre!("Script execution failed with status: {}", execute));
+        println!("This setup is not supported yet for Windows.");
     }
 
     debug!("Script executed successfully");
